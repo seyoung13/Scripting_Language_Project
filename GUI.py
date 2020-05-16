d4140425720bbca844b151b0ff13638454dbb8a8
seyoung <skview1201@gmail.com>
2020-05-17 00:12:11 +0900

    GUI 기본

--- GUI.py
+++ GUI.py
@@ -8,15 +8,16 @@ class GUI:
         window.title("목적지까지의 경로 구하기")
         window.geometry("800x600")
 
-
+        frame = Frame()
+        frame.pack()
 
         self.departure_button = Button(text='출발지')
         self.departure_button.pack(side=RIGHT)
         self.destination_button = Button(text='목적지')
         self.departure_button.pack(side=RIGHT)
 
-        self.scrollbar = Scrollbar()
-        self.scrollbar.pack()
+        self.scrollbar = Scrollbar(frame, width=200, borderwidth=100)
+        self.scrollbar.pack(side=LEFT)
 
         window.mainloop()
 
