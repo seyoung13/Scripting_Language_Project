d89d4dfb066526c28a7d7e3f97403fd5f98d99a9
seyoung <skview1201@gmail.com>
2020-05-27 03:14:00 +0900

    xml파싱 후 GUI에 옮겨오기 구현
    다음에 할 것
    장소 선택 후 엔트리에 집어넣기
    출발지, 목적지 따로 검색->
    장소 검색 후 출발지, 목적지 중 하나 선택하는 기능으로 변경

--- GUI.py
+++ GUI.py
@@ -1,11 +1,12 @@
 from tkinter import*
+import transferAPI
 
 
 class GUI:
 
     def __init__(self):
         self.window = Tk()
-        self.window.title("목적지까지의 경로 구하기")
+        self.window.title("대중교통 경로 정보")
 
         Label(self.window, text='출발지').grid(row=0, column=0)
         Label(self.window, text='목적지').grid(row=1, column=0)
@@ -14,30 +15,56 @@ class GUI:
         self.destination = Entry(self.window)
         self.destination.grid(row=1, column=1)
 
-        self.departure_button = Button(self.window, text='검색', command=self.search)
+        self.departure_button = Button(self.window, text='검색',
+                                       command=lambda: self.search_departure(self.departure.get()))
         self.departure_button.grid(row=0, column=2)
-        self.destination_button = Button(self.window, text='검색', command=self.search)
+        self.destination_button = Button(self.window, text='검색',
+                                         command=lambda: self.search_destination(self.destination.get()))
         self.destination_button.grid(row=1, column=2)
 
         Label(self.window, text='예상 경로').grid(row=2, column=0)
-        self.path = Scrollbar(self.window, width=200, borderwidth=100)
-        self.path.grid(row=2, column=1)
+        self.path_scroll = Scrollbar(self.window)
+        self.path_scroll.grid(row=2, column=2, sticky='w'+'n'+'s')
+        self.path_list = Listbox(self.window, yscrollcommand=self.path_scroll.set)
+        self.path_list.grid(row=2, column=1)
+
+        Label(self.window, text='이메일 주소').grid(row=3, column=0)
+        Entry(self.window).grid(row=3, column=1)
+        Button(self.window, text='경로 보내기').grid(row=3, column=2, stick='w')
 
         self.window.mainloop()
 
-    def search(self):
-        self.serach_window = Tk()
-        self.scrollbar = Scrollbar(self.serach_window)
-        self.scrollbar.pack()
-        self.listbox = Listbox(self.serach_window, yscrollcommand=self.scrollbar.set)
-        for line in range(5):
-            self.listbox.insert(line, str(line)+'aaa')
-        self.listbox.pack(side=LEFT)
-        self.scrollbar['command']=self.listbox.yview
-        Button(self.serach_window, text='확인', command=self.get_location).pack(side=RIGHT)
+    def search_departure(self, target_location):
+        self.searching_window = Tk()
+        self.result_scroll = Scrollbar(self.searching_window)
+        self.result_scroll.grid(row=0, column=1, sticky='w'+'n'+'s')
+        self.result_list = Listbox(self.searching_window, yscrollcommand=self.result_scroll.set)
 
-    def get_location(self):
-        self.serach_window.destroy()
+        self.location_name, self.lx, self.ly = transferAPI.search_location(target_location)
+
+        for i in range(len(self.location_name)):
+            self.result_list.insert(i, str(self.location_name[i]))
+        self.result_list.grid(row=0, column=0)
+        self.result_scroll['command'] = self.result_list.yview
+        Button(self.searching_window, text='확인', command=self.searching_window.destroy).grid(row=1, column=0)
+
+        self.departure.configure(text='ok')
+
+    def search_destination(self, target_location):
+        self.searching_window = Tk()
+        self.result_scroll = Scrollbar(self.searching_window)
+        self.result_scroll.grid(row=0, column=1, sticky='w' + 'n' + 's')
+        self.result_list = Listbox(self.searching_window, yscrollcommand=self.result_scroll.set)
+
+        self.location_name, self.lx, self.ly = transferAPI.search_location(target_location)
+
+        for i in range(len(self.location_name)):
+            self.result_list.insert(i, str(self.location_name[i]))
+        self.result_list.grid(row=0, column=0)
+        self.result_scroll['command'] = self.result_list.yview
+        Button(self.searching_window, text='확인', command=self.searching_window.destroy).grid(row=1, column=0)
+
+        self.destination.configure(text=str(self.result_list.curselection()))
 
 
 GUI()
---
+++ transferAPI.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from xml.dom.minidom import *
+import urllib
+import http.client
+from xml.etree import ElementTree
+
+# 대중교통 환승정보 서비스 API
+# 인증키
+key = '8zvGJaRxZX2%2Fr%2BWKMJ5jvbJstf1HYfg3vKs%2FTZaSBSXcJkPVoz7b1i4f4Dut5B8cZePVHygCmITymwMx2VeW0w%3D%3D'
+# 환승정보 api의 공통 url
+url = '/api/rest/pathinfo/getLocationInfo?ServiceKey='
+connection = http.client.HTTPConnection('ws.bus.go.kr')
+
+
+def search_location(keyword_string):
+    # 사용자가 입력한 문자열을 받아와서 api에 넘겨줌
+    global key, url, connection
+    location_name = urllib.parse.quote(keyword_string)
+    connection.request('GET', url+key+'&stSrch='+location_name)
+    req = connection.getresponse()
+    location_xml = req.read().decode('utf-8')
+    # api에서 이름, x좌표, y좌표 리스트를 받은 후 GUI에 넘겨줌
+    n, x, y = get_location_info(location_xml)
+    return n, x, y
+
+
+def get_location_info(api_xml):
+    # 사용자가 입력한 문자열을 포함한 장소들의 리스트를 반환
+    tree = ElementTree.fromstring(api_xml)
+
+    items = tree.iter('itemList')
+    name = []
+    gps_x, gps_y = [], []
+
+    for i in items:
+        name.append(i.find('poiNm').text)
+        gps_x.append(eval(i.find('gpsX').text)), gps_y.append(eval(i.find('gpsY').text))
+
+    return name, gps_x, gps_y
