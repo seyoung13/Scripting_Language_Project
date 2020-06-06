cfa94dec17d0afa6092aa6e1231c54b487480e95
seyoung <skview1201@gmail.com>
2020-06-06 09:59:41 +0900

    gps좌표 얻어오기
    api에서 환승경로 조회하는 법 만드는 중

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 from tkinter import*
 import transferAPI
+DEPARTURE, DESTINATION = range(2)
 
 
 class GUI:
@@ -10,16 +11,20 @@ class GUI:
 
         Label(self.window, text='출발지').grid(row=0, column=0)
         Label(self.window, text='목적지').grid(row=1, column=0)
-        self.departure = Entry(self.window)
-        self.departure.grid(row=0, column=1)
-        self.destination = Entry(self.window)
-        self.destination.grid(row=1, column=1)
 
+        self.departure = StringVar()
+        self.destination = StringVar()
+        self.start_x, self.start_y, self.end_x, self.end_y = [], [], [], []
+
+        self.departure_entry = Entry(self.window, textvariable=self.departure)
+        self.departure_entry.grid(row=0, column=1)
+        self.destination_entry = Entry(self.window, textvariable=self.destination)
+        self.destination_entry.grid(row=1, column=1)
         self.departure_button = Button(self.window, text='검색',
-                                       command=lambda: self.search_departure(self.departure.get()))
+                                       command=lambda: self.search(DEPARTURE, self.departure_entry.get()))
         self.departure_button.grid(row=0, column=2)
         self.destination_button = Button(self.window, text='검색',
-                                         command=lambda: self.search_destination(self.destination.get()))
+                                         command=lambda: self.search(DESTINATION, self.destination_entry.get()))
         self.destination_button.grid(row=1, column=2)
 
         Label(self.window, text='예상 경로').grid(row=2, column=0)
@@ -34,37 +39,42 @@ class GUI:
 
         self.window.mainloop()
 
-    def search_departure(self, target_location):
+    def search(self, classification_code, target_location):
         self.searching_window = Tk()
         self.result_scroll = Scrollbar(self.searching_window)
         self.result_scroll.grid(row=0, column=1, sticky='w'+'n'+'s')
         self.result_list = Listbox(self.searching_window, yscrollcommand=self.result_scroll.set)
 
-        self.location_name, self.lx, self.ly = transferAPI.search_location(target_location)
+        if target_location:
+            if classification_code == DEPARTURE:
+                self.start_x.clear(), self.start_y.clear()
+                location_name, self.start_x, self.start_y = transferAPI.search_location(target_location)
+            else:
+                self.end_x.clear(), self.end_y.clear()
+                location_name, self.end_x, self.end_y = transferAPI.search_location(target_location)
+            for i in range(len(location_name)):
+                self.result_list.insert(i, str(location_name[i]))
 
-        for i in range(len(self.location_name)):
-            self.result_list.insert(i, str(self.location_name[i]))
         self.result_list.grid(row=0, column=0)
         self.result_scroll['command'] = self.result_list.yview
-        Button(self.searching_window, text='확인', command=self.searching_window.destroy).grid(row=1, column=0)
+        Button(self.searching_window, text='확인', command=lambda:
+            self.set_location(classification_code, self.result_list, self.searching_window)).grid(row=1, column=0)
 
-        self.departure.configure(text='ok')
+    def set_location(self, classification_code, location_list, window, ):
+        if location_list.curselection():
+            i = location_list.curselection()
+            location_info = location_list.get(i)
+            if classification_code == DEPARTURE:
+                print(self.start_x[i[0]], self.start_y[i[0]])
+                self.departure.set(location_info)
+                # location_list[location_list.curselection()]
+            else:
+                print(self.end_x[i[0]], self.end_y[i[0]])
+                self.destination.set(location_info)
+        window.destroy()
 
-    def search_destination(self, target_location):
-        self.searching_window = Tk()
-        self.result_scroll = Scrollbar(self.searching_window)
-        self.result_scroll.grid(row=0, column=1, sticky='w' + 'n' + 's')
-        self.result_list = Listbox(self.searching_window, yscrollcommand=self.result_scroll.set)
-
-        self.location_name, self.lx, self.ly = transferAPI.search_location(target_location)
-
-        for i in range(len(self.location_name)):
-            self.result_list.insert(i, str(self.location_name[i]))
-        self.result_list.grid(row=0, column=0)
-        self.result_scroll['command'] = self.result_list.yview
-        Button(self.searching_window, text='확인', command=self.searching_window.destroy).grid(row=1, column=0)
-
-        self.destination.configure(text=str(self.result_list.curselection()))
+    def set_path_bus_n_subway(self, departure, destination):
+        pass
 
 
 GUI()
--- transferAPI.py
+++ transferAPI.py
@@ -18,9 +18,9 @@ def search_location(keyword_string):
     location_name = urllib.parse.quote(keyword_string)
     connection.request('GET', url+key+'&stSrch='+location_name)
     req = connection.getresponse()
-    location_xml = req.read().decode('utf-8')
+    xml = req.read().decode('utf-8')
     # api에서 이름, x좌표, y좌표 리스트를 받은 후 GUI에 넘겨줌
-    n, x, y = get_location_info(location_xml)
+    n, x, y = get_location_info(xml)
     return n, x, y
 
 
@@ -37,3 +37,18 @@ def get_location_info(api_xml):
         gps_x.append(eval(i.find('gpsX').text)), gps_y.append(eval(i.find('gpsY').text))
 
     return name, gps_x, gps_y
+
+
+def search_path_info_bus_n_subway(x1, y1, x2, y2):
+    global key, url, connection
+    connection.request('GET'+url+key+'&startX'+x1+'&startY'+y1+'&endX'+x2+'&endY')
+    req = connection.getresponse()
+    xml = req.read().decode('utf-8')
+
+
+def get_path(api_xml):
+    # 입력받은 출발지 ~ 목적지 사이의 경로 리스트 반환
+    tree = ElementTree.fromstring(api_xml)
+
+    items = tree.iter('itemList')
+    name = []
