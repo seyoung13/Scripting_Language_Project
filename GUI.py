bf06057bbe5b9ecfa3470d38fb81b03b43b04ea8
seyoung <skview1201@gmail.com>
2020-06-19 19:21:04 +0900

    검색창 한화면에 나오게 수정
    c++ 연동, 이메일, 텔레그램 봇, 배포 파일 하자

--- GUI.py
+++ GUI.py
@@ -5,6 +5,8 @@ import folium
 import folium.plugins
 DEPARTURE, DESTINATION = range(2)
 
+# 검색창 따로 만들지 말고 한 화면에 나오게!!!!
+# c++ 연동, 이메일 보내기, 텔레그램 봇 연동, 배포 파일 만들기
 
 class GUI:
 
@@ -20,23 +22,38 @@ class GUI:
         self.start_x, self.start_y, self.end_x, self.end_y = [], [], [], []
         # 리스트박스에서 선택한 장소의 인덱스 값을 가져오기 위한 변수
         self.selected_index = 0
+        self.code = DEPARTURE
 
-        self.departure_entry = Entry(self.window, textvariable=self.departure)
+        self.departure_entry = Entry(self.window, textvariable=self.departure, width=30)
         self.departure_entry.grid(row=0, column=1)
-        self.destination_entry = Entry(self.window, textvariable=self.destination)
+        self.destination_entry = Entry(self.window, textvariable=self.destination, width=30)
         self.destination_entry.grid(row=1, column=1)
-        self.departure_button = Button(self.window, text='검색', padx=8,
+        self.departure_button = Button(self.window, text='검색', padx=12,
                                        command=lambda: self.search(DEPARTURE, self.departure_entry.get()))
         self.departure_button.grid(row=0, column=2)
-        self.destination_button = Button(self.window, text='검색', padx=8,
+        self.destination_button = Button(self.window, text='검색', padx=12,
                                          command=lambda: self.search(DESTINATION, self.destination_entry.get()))
         self.destination_button.grid(row=1, column=2)
 
-        Button(self.window, text='버스', command=self.set_path_bus).grid(row=2, column=1, sticky='w')
-        Button(self.window, text='지하철', command=self.set_path_sub).grid(row=2, column=1)
-        Button(self.window, text='혼합', command=self.set_path_bus_n_subway).grid(row=2, column=1, sticky='e')
+        self.result_scroll = Scrollbar(self.window)
+        self.result_scroll.grid(row=3, column=2, sticky='w'+'n'+'s')
+        self.result_list = Listbox(self.window, yscrollcommand=self.result_scroll.set, width=30)
 
-        Label(self.window, text='예상 경로').grid(row=2, column=0, pady=10)
+        self.result_list.grid(row=3, column=1)
+        self.result_scroll['command'] = self.result_list.yview
+        Button(self.window, text='확인', command=lambda:
+            self.set_location(self.code,  self.result_list)).grid(row=4, column=1, sticky='w')
+
+        Button(self.window, text='지도 보기', command=lambda:
+            self.show_location_map(self.code, self.result_list)).grid(row=4, column=1, sticky='e')
+
+        Label(self.window).grid(column=3, padx=20)
+
+        Button(self.window, text='버스', command=self.set_path_bus).grid(row=0, column=4)
+        Button(self.window, text='지하철', command=self.set_path_sub).grid(row=0, column=5)
+        Button(self.window, text='버스+지하철', command=self.set_path_bus_n_subway).grid(row=0, column=6)
+
+        Label(self.window, text='예상 경로').grid(row=1, column=5, pady=10)
 
         #Label(self.window, text='이메일 주소').grid(row=3, column=0, padx=10, pady=5)
         #Entry(self.window).grid(row=3, column=1)
@@ -45,30 +62,20 @@ class GUI:
         self.window.mainloop()
 
     def search(self, classification_code, target_location):
-        search_window = Tk()
-        result_scroll = Scrollbar(search_window)
-        result_scroll.grid(row=0, column=1, sticky='w'+'n'+'s')
-        result_list = Listbox(search_window, yscrollcommand=result_scroll.set)
+        self.code = classification_code
 
+        self.result_list.delete(0, 'end')
         if target_location:
-            if classification_code == DEPARTURE:
+            if self.code == DEPARTURE:
                 self.start_x.clear(), self.start_y.clear()
                 location_name, self.start_x, self.start_y = transferAPI.search_location(target_location)
             else:
                 self.end_x.clear(), self.end_y.clear()
                 location_name, self.end_x, self.end_y = transferAPI.search_location(target_location)
             for i in range(len(location_name)):
-                result_list.insert(i, str(location_name[i]))
+                self.result_list.insert(i, str(location_name[i]))
 
-        result_list.grid(row=0, column=0)
-        result_scroll['command'] = result_list.yview
-        Button(search_window, text='확인', command=lambda:
-            self.set_location(classification_code, result_list, search_window)).grid(row=1, column=0)
-
-        Button(search_window, text='지도 보기', command=lambda:
-            self.show_location(classification_code, result_list)).grid(row=1, column=1)
-
-    def set_location(self, classification_code, location_list, window):
+    def set_location(self, classification_code, location_list):
         if location_list.curselection():
             self.selected_index = location_list.curselection()[0]
             location_info = location_list.get(self.selected_index)
@@ -76,7 +83,6 @@ class GUI:
                 self.departure.set(location_info)
             else:
                 self.destination.set(location_info)
-        window.destroy()
 
     def set_path_bus(self):
         path_window = Tk()
@@ -102,7 +108,7 @@ class GUI:
                 path_list.insert(i, path)
 
             Button(path_window, text='지도 보기', command=lambda:
-                self.show_path(path_list, departure_name, fx, fy, destination_name, tx, ty)).grid(row=1, column=1)
+                self.show_path_map(path_list, departure_name, fx, fy, destination_name, tx, ty)).grid(row=1, column=1)
 
     def set_path_sub(self):
         path_window = Tk()
@@ -128,7 +134,7 @@ class GUI:
                 path_list.insert(i, path)
 
             Button(path_window, text='지도 보기', command=lambda:
-                self.show_path(path_list, departure_name, fx, fy, destination_name, tx, ty)).grid(row=1, column=1)
+                self.show_path_map(path_list, departure_name, fx, fy, destination_name, tx, ty)).grid(row=1, column=1)
 
     def set_path_bus_n_subway(self):
         path_window = Tk()
@@ -154,9 +160,9 @@ class GUI:
                 path_list.insert(i, path)
 
             Button(path_window, text='지도 보기', command=lambda:
-                self.show_path(path_list, departure_name, fx, fy, destination_name, tx, ty)).grid(row=1, column=1)
+                self.show_path_map(path_list, departure_name, fx, fy, destination_name, tx, ty)).grid(row=1, column=1)
 
-    def show_location(self, classification_code, location_list):
+    def show_location_map(self, classification_code, location_list):
         if location_list.curselection():
             self.selected_index = location_list.curselection()[0]
             location_info = location_list.get(self.selected_index)
@@ -174,7 +180,7 @@ class GUI:
             map_osm.save('osm.html')
             webbrowser.open_new('osm.html')
 
-    def show_path(self, path_list, departure_name, fx, fy, destination_name, tx, ty):
+    def show_path_map(self, path_list, departure_name, fx, fy, destination_name, tx, ty):
         if path_list.curselection():
             i = path_list.curselection()[0]
 
